2673827286b710abc4831d774ebc8686d15a17e9
MultySerey <multyserey@gmail.com>
2024-04-11 18:03:28 +0500

    dialogic addon

--- scripts/CharFox.gd
+++ scripts/CharFox.gd
@@ -1,19 +1,23 @@
 extends CharacterBody3D
 
 var text_toggle: bool=false
+var timeline: DialogicTimeline = DialogicTimeline.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var events: Array = """
+	join fox 1
+	fox (fox): Теперь я могу брать текст из кода!
+	- Круто!
+	fox (fox): Пока.
+		
+	""".split('\n')
+	timeline.events= events
+	timeline.events_processed=true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func interact():
-	if text_toggle:
-		$Label3D.show()
-		text_toggle=false
-	else:
-		$Label3D.hide()
-		text_toggle=true
+	Dialogic.start(timeline)
